d8ce7028e4b07b1120ffa9d781ea14efc49e6873
limaohui <limaohui1993@outlook.com>
2023-03-11 21:56:52 +0800

    初步配置

---
+++ src/router/index.ts
@@ -0,0 +1,20 @@
+import {createRouter,createWebHashHistory,RouteRecordRaw} from 'vue-router';
+
+const routes:RouteRecordRaw[]=[
+    {
+        path:"/home",
+        name:"home",
+        component:()=>import('../views/home/index.vue'),
+    },
+    {
+        path:"/login",
+        name:"login",
+        component:()=>import('../views/login/index.vue'),
+    },
+]
+
+const router=createRouter({
+    history:createWebHashHistory(),
+    routes
+})
+export default router
---
+++ src/views/home/index.vue
@@ -0,0 +1,10 @@
+<template>
+ <div>
+    <h1>首页</h1>
+ </div>
+</template>
+<script language="ts">
+</script>
+<style lang='scss' scoped>
+ 
+</style>
